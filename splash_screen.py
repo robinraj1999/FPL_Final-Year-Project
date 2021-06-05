be345468321aa63e077e5a4ca0fb58684fa3c80c
robinraj1999 <68849138+robinraj1999@users.noreply.github.com>
2021-06-05 17:09:30 +0400

    Update splash_screen.py

--- splash_screen.py
+++ splash_screen.py
@@ -9,44 +9,27 @@ import fastprogress
 import sys
 sys.path.append("..")
 
-col1, col2, col3 = st.beta_columns([1,6,2])
 
-with col1:
-	st.write("")
+
+st.title('Final Year Project')
+st.markdown("""---""")
+st.markdown('''
+    <a href="https://share.streamlit.io/robinraj1999/fpl-optimal-team-optimizer/main/mywork1.py">
+	<img src="https://www.chapsandco.ae/wp-content/uploads/FantasyLaunch-TakeoverEditorial.jpg" width="400" />
+    </a>''',
+    unsafe_allow_html=True
+)
+st.title('Generate Optimal Team')
+st.markdown("""---""")
+
+st.markdown('''
+    <a href="https://www.google.com">
+	<img src="https://resources.premierleague.com/premierleague/photo/2020/06/08/e622b94f-6ad4-43d4-a1c2-226eb8b2f163/FantasyLaunch-EditorialLead-Graphic1-002-.png" width="400" />
+    </a>''',
+    unsafe_allow_html=True
+)
+st.title('Player Statistics')
 	
 
-with col2:
-	st.title('Final Year Project')
-	st.markdown("""---""")
-	st.markdown('''
-	    <a href="https://share.streamlit.io/robinraj1999/fpl-optimal-team-optimizer/main/mywork1.py">
-	        <img src="https://www.chapsandco.ae/wp-content/uploads/FantasyLaunch-TakeoverEditorial.jpg" width="400" />
-	    </a>''',
-	    unsafe_allow_html=True
-	)
-	
-	st.markdown("""---""")
-   
-	st.markdown('''
-	    <a href="https://www.google.com">
-	        <img src="https://resources.premierleague.com/premierleague/photo/2020/06/08/e622b94f-6ad4-43d4-a1c2-226eb8b2f163/FantasyLaunch-EditorialLead-Graphic1-002-.png" width="400" />
-	    </a>''',
-	    unsafe_allow_html=True
-	)
-	
-with col3:
-	st.write("")
-	st.markdown('#')	
-	st.markdown('#')
-	st.markdown('#')
-	st.title('Generate Optimal Team')
-	st.markdown('#')
-	st.markdown('#')
-	st.markdown('#')
-	st.markdown('#')
-	st.markdown('#')
-	st.markdown('#')
-	
-	st.title('Choose Team Manually')
 
    
